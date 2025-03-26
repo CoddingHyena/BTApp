f2da80a7020d831f2675131f99e2c97613108559
Serge49 <Serge49@mail.ru>
2025-03-26 16:28:52 +0300

    linter commit

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 2,
+    endOfLine: 'auto', // Решает проблемы с CRLF и LF
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+  };
